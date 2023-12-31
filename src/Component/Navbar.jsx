3a00bde6564dc5052406f4feb1dde264199a34c8
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-12-31 14:26:16 +0530

    styling navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,25 +2,26 @@ import logo from '/logo.png'
 function Navbar(){
     return(
     //    main div start here
-         <div>
-
+         <div className='w-full h-24 flex items-center justify-between gap-14'>
+          {/*  border-red-400 border-4 */}
 
             {/* logo div */}
-            <div>
-                <img src={logo}></img>
+            <div className='ml-8'>
+                <img className="w-48 "src={logo}></img>
             </div>
 
             {/*logo div ends here  */}
- \
+ 
 
             {/* items div Start here */}
 
    
-            <div>
-
+            <div className=' w-9/12 h-full flex  justify-between items-center'>
+                 {/*  border-pink-400 border-4 */}
                 {/* List div  start here*/}
-                <div>
-                <ul>
+                <div className=' ml-48'>
+                    {/* border-green-300 border-4 */}
+                <ul className='flex gap-8'>
                     <li>Pricing</li>
                     <li>Add-ons</li>
                     <li>FAQs</li>
@@ -33,8 +34,9 @@ function Navbar(){
 
 
                  {/* button div start here */}
-                <div>
-                    <button>Start Here</button>
+                <div className=" mr-14 w-28 h-12 bg-green-300 rounded">
+                    {/* 'border-orange-300 border-4 */}
+                    <button className='pl-5 pt-3'>Start Here</button>
                 </div>
                 {/* button div ends here */}
 
