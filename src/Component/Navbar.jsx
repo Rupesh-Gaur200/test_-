0095c1994fe002141b40b01b0031984142ecb514
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-12-31 22:02:32 +0530

    making navbar responsive

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,12 +2,12 @@ import logo from '/logo.png'
 function Navbar(){
     return(
     //    main div start here
-         <div className='w-full h-24 flex items-center justify-between gap-14'>
+         <div className=' w-full h-24 flex items-center justify-between gap-14'>
           {/*  border-red-400 border-4 */}
 
             {/* logo div */}
-            <div className='ml-12'>
-                <img className="w-48 "src={logo}></img>
+            <div className='shrink-0 w-48  ml-14'>
+                <img className="shrink-0 "src={logo}></img>
             </div>
 
             {/*logo div ends here  */}
@@ -16,10 +16,10 @@ function Navbar(){
             {/* items div Start here */}
 
    
-            <div className=' w-9/12 h-full flex  justify-between items-center'>
+            <div className=' md:w-9/12 h-full flex  justify-between items-center gap-12'>
                  {/*  border-pink-400 border-4 */}
                 {/* List div  start here*/}
-                <div className=' ml-48 mt-1'>
+                <div className='hidden md:block ml-48 mt-1 '>
                     {/* border-green-300 border-4 */}
                 <ul className='flex gap-10'>
                     <li>Pricing</li>
@@ -34,7 +34,7 @@ function Navbar(){
 
 
                  {/* button div start here */}
-                <div className="  mr-14 w-28 h-12 bg-green-300 rounded">
+                <div className="   mr-14 w-28 h-12 bg-green-300 rounded">
                     {/* 'border-orange-300 border-4 */}
                     <button className='mt-3 ml-5 '>Start Here</button>
                 </div>
