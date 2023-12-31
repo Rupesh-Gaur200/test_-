e412992fd1bcb1348d4dc6beb4eaffbf05a506ef
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-12-31 14:39:41 +0530

    Centering the items

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar(){
           {/*  border-red-400 border-4 */}
 
             {/* logo div */}
-            <div className='ml-8'>
+            <div className='ml-12'>
                 <img className="w-48 "src={logo}></img>
             </div>
 
@@ -19,9 +19,9 @@ function Navbar(){
             <div className=' w-9/12 h-full flex  justify-between items-center'>
                  {/*  border-pink-400 border-4 */}
                 {/* List div  start here*/}
-                <div className=' ml-48'>
+                <div className=' ml-48 mt-1'>
                     {/* border-green-300 border-4 */}
-                <ul className='flex gap-8'>
+                <ul className='flex gap-10'>
                     <li>Pricing</li>
                     <li>Add-ons</li>
                     <li>FAQs</li>
@@ -34,9 +34,9 @@ function Navbar(){
 
 
                  {/* button div start here */}
-                <div className=" mr-14 w-28 h-12 bg-green-300 rounded">
+                <div className="  mr-14 w-28 h-12 bg-green-300 rounded">
                     {/* 'border-orange-300 border-4 */}
-                    <button className='pl-5 pt-3'>Start Here</button>
+                    <button className='mt-3 ml-5 '>Start Here</button>
                 </div>
                 {/* button div ends here */}
 
